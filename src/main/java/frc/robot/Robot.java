0214b810596079696ebccd51c9f897676bf44576
chadlee1 <clee@set-works.com>
2019-07-06 14:02:41 -0500

    Deleted Comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -120,7 +120,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
-    //hi my name is chad. please delete this later
+  
   }
 
   /**
