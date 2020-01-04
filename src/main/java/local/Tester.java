16c0d378ae591a7632898b06e30667cb51a07b64
Brian Donald <bdonald@set-works.com>
2020-01-04 17:47:58 -0600

    Add configuration and logging capability

---
+++ src/main/java/local/Tester.java
@@ -0,0 +1,9 @@
+package local;
+
+import frc.robot.helpers.RLogger;
+
+public class Tester {
+    public static void main(String[] args) throws Exception {
+        RLogger.debug("BEPISBEPISBEPIS");
+    }
+}
