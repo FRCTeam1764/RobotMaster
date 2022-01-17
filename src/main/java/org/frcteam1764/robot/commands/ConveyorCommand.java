79738b0659427828689295b31ead3ee8f0a7d37f
Team 1764 <first1764@gmail.com>
2022-01-17 14:08:03 -0600

    PreCode Conveyor

---
+++ src/main/java/org/frcteam1764/robot/commands/ConveyorCommand.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.frcteam1764.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ConveyorCommand extends CommandBase {
+  /** Creates a new ConveyorCommand. */
+ Conveyor 
+
+  public ConveyorCommand(double conveyorSpeed) {
+    conveyor = new conveyor(conveyorSpeed);
+
+    addRequirements(conveyor);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    conveyor.conveyerOn();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    conveyor.conveyerOff();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
