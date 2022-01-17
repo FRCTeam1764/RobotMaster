e5e16eac9d58aaee6494ad084696d7e0b2620f6c
Team 1764 <first1764@gmail.com>
2022-01-17 14:08:03 -0600

    PreCode Conveyor

---
+++ src/main/java/org/frcteam1764/robot/subsystems/Conveyor.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.frcteam1764.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+/** Add your docs here. I wont*/
+public class Conveyor extends Subsystem {
+ WPI_TalonFX CVmotor = new WPI_TalonFX(69420);
+ 
+ double conveyorSpeed;
+
+ public Conveyor(double conveyorSpeed){
+  this.conveyorSpeed = conveyorSpeed;
+ }
+
+  public void conveyorOn() {
+    CVmotor.set(ControlMode.PercentOutput, conveyorSpeed);
+  }
+  public void conveyerOff() {
+    CVmotor.set(ControlMode.PercentOutput, 0);
+  }
+  
+}
