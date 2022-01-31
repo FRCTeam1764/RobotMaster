89cdfc48a8ec9bc4c12d60d8b5332ec2e430b3e8
Team 1764 <first1764@gmail.com>
2022-01-30 19:17:56 -0600

    -changing some CAN IDs to match how they were assigned

--- src/main/java/org/frcteam1764/robot/constants/RobotConstants.java
+++ src/main/java/org/frcteam1764/robot/constants/RobotConstants.java
@@ -4,7 +4,7 @@ public class RobotConstants {
     
     public static final int INTAKE_MOTOR = 60;
 
-    public static final int SHOOTER_MASTER_MOTOR = 30;
+    public static final int SHOOTER_MASTER_MOTOR = 32;
     public static final int SHOOTER_FOLLOWER_MOTOR = 31;
 
     public static final int ELEVATOR_MOTOR = 20;
@@ -16,17 +16,17 @@ public class RobotConstants {
 
     public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 6;
     public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 7;
-    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 8;
-    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 9;
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 9;
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 8;
 
     public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 10;
     public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 11;
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 12;
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 13;
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 13;
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 12;
 
-    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 14;
-    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 15;
-    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 16;
+    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 15;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 16;
+    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 18;
     public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 17;
 
     // In degrees
